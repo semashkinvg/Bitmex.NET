b738e577372d0f31965b8f3d8938ae88409b7873
semashkin.vg <semashkin.vg@gmail.com>
2018-05-10 10:41:52 +0400

    fixed bug #2

--- Bitmex.NET/Dtos/TradeBucketedDto.cs
+++ Bitmex.NET/Dtos/TradeBucketedDto.cs
@@ -1,51 +1,46 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bitmex.NET.Dtos
 {
-    public class TradeBucketedDto
-    {
-        [JsonProperty("timestamp")]
-        public System.DateTimeOffset Timestamp { get; set; }
+	public class TradeBucketedDto
+	{
+		[JsonProperty("timestamp")]
+		public System.DateTimeOffset Timestamp { get; set; }
 
-        [JsonProperty("symbol")]
-        public string Symbol { get; set; }
+		[JsonProperty("symbol")]
+		public string Symbol { get; set; }
 
-        [JsonProperty("open")]
-        public long Open { get; set; }
+		[JsonProperty("open")]
+		public long Open { get; set; }
 
-        [JsonProperty("high")]
-        public long High { get; set; }
+		[JsonProperty("high")]
+		public long High { get; set; }
 
-        [JsonProperty("low")]
-        public long Low { get; set; }
+		[JsonProperty("low")]
+		public long Low { get; set; }
 
-        [JsonProperty("close")]
-        public long Close { get; set; }
+		[JsonProperty("close")]
+		public long Close { get; set; }
 
-        [JsonProperty("trades")]
-        public long Trades { get; set; }
+		[JsonProperty("trades")]
+		public long Trades { get; set; }
 
-        [JsonProperty("volume")]
-        public long Volume { get; set; }
+		[JsonProperty("volume")]
+		public long Volume { get; set; }
 
-        [JsonProperty("vwap")]
-        public long Vwap { get; set; }
+		[JsonProperty("vwap")]
+		public long? Vwap { get; set; }
 
-        [JsonProperty("lastSize")]
-        public long LastSize { get; set; }
+		[JsonProperty("lastSize")]
+		public long? LastSize { get; set; }
 
-        [JsonProperty("turnover")]
-        public long Turnover { get; set; }
+		[JsonProperty("turnover")]
+		public long Turnover { get; set; }
 
-        [JsonProperty("homeNotional")]
-        public long HomeNotional { get; set; }
+		[JsonProperty("homeNotional")]
+		public long HomeNotional { get; set; }
 
-        [JsonProperty("foreignNotional")]
-        public long ForeignNotional { get; set; }
-    }
+		[JsonProperty("foreignNotional")]
+		public long ForeignNotional { get; set; }
+	}
 }
