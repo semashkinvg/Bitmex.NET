72ad15d4f53224e34af60f02b299e90e129d6a03
Matt Craig <matt.craig@ninemilefinancial.com>
2018-07-18 16:36:08 +0800

    Make nonce generation thread-safe (& minor optimisations)

--- Bitmex.NET/Authorization/NonceProvider.cs
+++ Bitmex.NET/Authorization/NonceProvider.cs
@@ -7,21 +7,14 @@ namespace Bitmex.NET.Authorization
     {
         long GetNonce();
     }
+
     public class NonceProvider : INonceProvider
     {
-        private static long _lastNonce = 0;
+        private static long _lastNonce = DateTime.UtcNow.Ticks - new DateTime(2010, 1, 1).Ticks;
+
         public long GetNonce()
         {
-            var yearBegin = new DateTime(1970, 1, 1);
-            var newNonce = Convert.ToInt64(Math.Floor((DateTime.UtcNow - yearBegin).TotalSeconds));
-            if (_lastNonce < newNonce)
-            {
-                Interlocked.Exchange(ref _lastNonce, newNonce);
-                return _lastNonce;
-            }
-
-            Interlocked.Increment(ref _lastNonce);
-            return _lastNonce;
+            return Interlocked.Increment(ref _lastNonce);
         }
     }
 }
