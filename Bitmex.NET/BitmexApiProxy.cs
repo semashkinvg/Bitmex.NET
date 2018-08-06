f1f43664a57e37e4d2c25e8c772de7d37dbe5ba8
Matt Craig <matt.craig@ninemilefinancial.com>
2018-08-06 22:50:07 +0800

    Resolve conflicts when merging latest

--- Bitmex.NET/BitmexApiProxy.cs
+++ Bitmex.NET/BitmexApiProxy.cs
@@ -1,6 +1,5 @@
 ﻿using Bitmex.NET.Authorization;
 using Bitmex.NET.Dtos;
-using Bitmex.NET.Logging;
 using Bitmex.NET.Models;
 using Newtonsoft.Json;
 using System;
@@ -8,129 +7,98 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Bitmex.NET.Logging;
 
 namespace Bitmex.NET
 {
     public class BitmexApiProxy : IBitmexApiProxy
     {
         private static readonly ILog Log = LogProvider.GetCurrentClassLogger();
-        private string CurrentHost => $"https://{Environments.Values[_bitmexAuthorization.BitmexEnvironment]}";
 
         private readonly IBitmexAuthorization _bitmexAuthorization;
         private readonly IExpiresTimeProvider _expiresTimeProvider;
         private readonly ISignatureProvider _signatureProvider;
+
+        private readonly HttpClient _httpClient;
 
         public BitmexApiProxy(IBitmexAuthorization bitmexAuthorization, IExpiresTimeProvider expiresTimeProvider, ISignatureProvider signatureProvider)
         {
             _bitmexAuthorization = bitmexAuthorization;
             _expiresTimeProvider = expiresTimeProvider;
             _signatureProvider = signatureProvider;
+
+            _httpClient = new HttpClient { BaseAddress = new Uri($"https://{Environments.Values[_bitmexAuthorization.BitmexEnvironment]}") };
+
+            _httpClient.DefaultRequestHeaders.Add("api-key", _bitmexAuthorization.Key ?? string.Empty);
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/javascript"));
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/javascript"));
         }
+
         public BitmexApiProxy(IBitmexAuthorization bitmexAuthorization) : this(bitmexAuthorization, new ExpiresTimeProvider(), new SignatureProvider())
         {
         }
 
-        public async Task<string> Get(string action, IQueryStringParams parameters)
+        public Task<string> Get(string action, IQueryStringParams parameters)
         {
-            using (var httpClient = new HttpClient())
-            {
-                var currentHost = CurrentHost;
-                var query = parameters?.ToQueryString() ?? string.Empty;
-                query = string.IsNullOrWhiteSpace(query) ? query : "?" + query;
-                var url = $"/api/v1/{action}{query}";
-                Auth(httpClient, url, HttpMethods.GET);
-                Log.Debug($"GET {url}");
-                var resp = await httpClient.GetAsync(currentHost + url);
-                await HandleErrorResp(resp, url, "GET");
-                var respAsString = await resp.Content.ReadAsStringAsync();
-                Log.Debug($"GET {url} resp:{respAsString}");
-                return respAsString;
-            }
+            var query = parameters?.ToQueryString() ?? string.Empty;
+            var request = new HttpRequestMessage(HttpMethod.Get, GetUrl(action) + (string.IsNullOrWhiteSpace(query) ? string.Empty : "?" + query));
+
+            return SendAndGetResponseAsync(request);
         }
 
-        public async Task<string> Post(string action, IJsonQueryParams parameters)
+        public Task<string> Post(string action, IJsonQueryParams parameters) => SendAndGetResponseAsync(HttpMethod.Post, action, parameters);
+
+        public Task<string> Put(string action, IJsonQueryParams parameters) => SendAndGetResponseAsync(HttpMethod.Put, action, parameters);
+
+        public Task<string> Delete(string action, IJsonQueryParams parameters) => SendAndGetResponseAsync(HttpMethod.Delete, action, parameters);
+
+        private Task<string> SendAndGetResponseAsync(HttpMethod method, string action, IJsonQueryParams parameters)
         {
-            using (var httpClient = new HttpClient())
+            var content = parameters?.ToJson() ?? string.Empty;
+            var request = new HttpRequestMessage(method, GetUrl(action))
             {
-                var url = "/api/v1/" + action;
-                var postData = parameters?.ToJson() ?? string.Empty;
-                Auth(httpClient, url, HttpMethods.POST, postData);
-                Log.Debug($"POST {url}");
-                var resp = await httpClient.PostAsync(CurrentHost + url, new StringContent(postData, Encoding.UTF8, "application/json"));
-                await HandleErrorResp(resp, url, "POST");
-                var respAsString = await resp.Content.ReadAsStringAsync();
-                Log.Debug($"POST {url} resp:{respAsString}");
-                return respAsString;
-            }
-        }
-        public async Task<string> Put(string action, IJsonQueryParams parameters)
-        {
-            using (var httpClient = new HttpClient())
-            {
-                var url = "/api/v1/" + action;
-                var postData = parameters?.ToJson() ?? string.Empty;
-                Auth(httpClient, url, HttpMethods.PUT, postData);
-                Log.Debug($"PUT {url}");
-                var resp = await httpClient.PutAsync(CurrentHost + url, new StringContent(postData, Encoding.UTF8, "application/json"));
-                await HandleErrorResp(resp, url, "PUT");
-                var respAsString = await resp.Content.ReadAsStringAsync();
-                Log.Debug($"PUT {url} resp:{respAsString}");
-                return respAsString;
-            }
-        }
-        public async Task<string> Delete(string action, IJsonQueryParams parameters)
-        {
-            using (var httpClient = new HttpClient())
-            {
-                var url = "/api/v1/" + action;
-                var postData = parameters?.ToJson() ?? string.Empty;
-                Auth(httpClient, url, HttpMethods.DELETE, postData);
-                HttpRequestMessage request = new HttpRequestMessage
-                {
-                    Content = new StringContent(postData, Encoding.UTF8, "application/json"),
-                    Method = HttpMethod.Delete,
-                    RequestUri = new Uri(CurrentHost + url)
-                };
-                var resp = await httpClient.SendAsync(request);
-                await HandleErrorResp(resp, url, "DELETE");
-                var respAsString = await resp.Content.ReadAsStringAsync();
-                Log.Debug($"PUT {url} resp:{respAsString}");
-                return respAsString;
-            }
+                Content = new StringContent(content, Encoding.UTF8, "application/json")
+            };
+
+            return SendAndGetResponseAsync(request, content);
         }
 
-        private void Auth(HttpClient httpClient, string url, HttpMethods httpMethod, string @params = "")
+        private async Task<string> SendAndGetResponseAsync(HttpRequestMessage request, string @params = null)
         {
-            var key = _bitmexAuthorization.Key ?? string.Empty;
-            var secret = _bitmexAuthorization.Secret ?? string.Empty;
-            var expiresTime = _expiresTimeProvider.Get().ToString();
-            var signatureString = _signatureProvider.CreateSignature(secret, $"{httpMethod}{url}{expiresTime}{@params}");
-            httpClient.DefaultRequestHeaders.Add("api-expires", expiresTime);
-            httpClient.DefaultRequestHeaders.Add("api-key", key);
-            httpClient.DefaultRequestHeaders.Add("api-signature", signatureString);
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/javascript"));
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/javascript"));
-        }
+            Sign(request, @params);
 
-        private async Task HandleErrorResp(HttpResponseMessage resp, string url, string method)
-        {
-            if (!resp.IsSuccessStatusCode)
+            Log.Debug($"{request.Method} {request.RequestUri}");
+
+            var response = await _httpClient.SendAsync(request);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Log.Debug($"{request.Method} {request.RequestUri} {(response.IsSuccessStatusCode ? "resp" : "errorResp")}:{responseString}");
+
+            if (!response.IsSuccessStatusCode)
             {
-                var errorResp = await resp.Content.ReadAsStringAsync();
-                Log.Debug($"{method} {url} errorResp:{errorResp}");
                 try
                 {
-                    var error = JsonConvert.DeserializeObject<BitmexApiError>(errorResp);
-                    throw new BitmexApiException(error);
+                    throw new BitmexApiException(JsonConvert.DeserializeObject<BitmexApiError>(responseString));
                 }
                 catch (JsonReaderException)
                 {
-                    throw new BitmexApiException(errorResp);
+                    throw new BitmexApiException(responseString);
                 }
             }
+
+            return responseString;
         }
+
+        private void Sign(HttpRequestMessage request, string @params)
+        {
+            request.Headers.Add("api-expires", _expiresTimeProvider.Get().ToString());
+            request.Headers.Add("api-signature", _signatureProvider.CreateSignature(_bitmexAuthorization.Secret ?? string.Empty,
+                $"{request.Method}{request.RequestUri}{_expiresTimeProvider.Get().ToString()}{@params}"));
+        }
+
+        private static string GetUrl(string action) => "/api/v1/" + action;
     }
 }
