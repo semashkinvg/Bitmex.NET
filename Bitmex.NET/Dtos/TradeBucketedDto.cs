031776be515d120c5651f5e4272c0836428fc384
semashkin.vg <semashkin.vg@gmail.com>
2018-11-13 19:36:44 +0400

    fixed due to possible resonse from server {"timestamp":"2016-05-05T04:01:00.000Z","symbol":"XBTUSD","open":null,"high":null,"low":null,"close":null,"trades":0,"volume":0,"vwap":null,"lastSize":null,"turnover":0,"homeNotional":0,"foreignNotional":0}

--- Bitmex.NET/Dtos/TradeBucketedDto.cs
+++ Bitmex.NET/Dtos/TradeBucketedDto.cs
@@ -2,37 +2,37 @@
 
 namespace Bitmex.NET.Dtos
 {
-	public class TradeBucketedDto
-	{
-		[JsonProperty("timestamp")]
-		public System.DateTimeOffset Timestamp { get; set; }
+    public class TradeBucketedDto
+    {
+        [JsonProperty("timestamp")]
+        public System.DateTimeOffset Timestamp { get; set; }
 
-		[JsonProperty("symbol")]
-		public string Symbol { get; set; }
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
 
-		[JsonProperty("open")]
-		public decimal Open { get; set; }
+        [JsonProperty("open")]
+        public decimal? Open { get; set; }
 
-		[JsonProperty("high")]
-		public decimal High { get; set; }
+        [JsonProperty("high")]
+        public decimal? High { get; set; }
 
-		[JsonProperty("low")]
-		public decimal Low { get; set; }
+        [JsonProperty("low")]
+        public decimal? Low { get; set; }
 
-		[JsonProperty("close")]
-		public decimal Close { get; set; }
+        [JsonProperty("close")]
+        public decimal? Close { get; set; }
 
 		[JsonProperty("trades")]
 		public decimal Trades { get; set; }
 
-		[JsonProperty("volume")]
-		public decimal Volume { get; set; }
+        [JsonProperty("volume")]
+        public decimal? Volume { get; set; }
 
-		[JsonProperty("vwap")]
-		public decimal? Vwap { get; set; }
+        [JsonProperty("vwap")]
+        public decimal? Vwap { get; set; }
 
-		[JsonProperty("lastSize")]
-		public decimal? LastSize { get; set; }
+        [JsonProperty("lastSize")]
+        public decimal? LastSize { get; set; }
 
 		[JsonProperty("turnover")]
 		public decimal Turnover { get; set; }
