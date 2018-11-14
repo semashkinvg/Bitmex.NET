9867b248077b7b323de74e73ca64adea56d2ff6d
semashkin.vg <semashkin.vg@gmail.com>
2018-11-14 13:06:18 +0400

    fixed #22

--- Bitmex.NET/Dtos/TradeDto.cs
+++ Bitmex.NET/Dtos/TradeDto.cs
@@ -2,36 +2,36 @@
 
 namespace Bitmex.NET.Dtos
 {
-	public class TradeDto
-	{
-		[JsonProperty("timestamp")]
-		public System.DateTimeOffset Timestamp { get; set; }
+    public class TradeDto
+    {
+        [JsonProperty("timestamp")]
+        public System.DateTimeOffset Timestamp { get; set; }
 
-		[JsonProperty("symbol")]
-		public string Symbol { get; set; }
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
 
-		[JsonProperty("side")]
-		public string Side { get; set; }
+        [JsonProperty("side")]
+        public string Side { get; set; }
 
-		[JsonProperty("size")]
-		public decimal Size { get; set; }
+        [JsonProperty("size")]
+        public decimal Size { get; set; }
 
-		[JsonProperty("price")]
-		public decimal Price { get; set; }
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
 
-		[JsonProperty("tickDirection")]
-		public string TickDirection { get; set; }
+        [JsonProperty("tickDirection")]
+        public string TickDirection { get; set; }
 
-		[JsonProperty("trdMatchID")]
-		public string TrdMatchId { get; set; }
+        [JsonProperty("trdMatchID")]
+        public string TrdMatchId { get; set; }
 
-		[JsonProperty("grossValue")]
-		public decimal GrossValue { get; set; }
+        [JsonProperty("grossValue")]
+        public decimal? GrossValue { get; set; }
 
-		[JsonProperty("homeNotional")]
-		public decimal HomeNotional { get; set; }
+        [JsonProperty("homeNotional")]
+        public decimal HomeNotional { get; set; }
 
-		[JsonProperty("foreignNotional")]
-		public decimal ForeignNotional { get; set; }
-	}
+        [JsonProperty("foreignNotional")]
+        public decimal ForeignNotional { get; set; }
+    }
 }
